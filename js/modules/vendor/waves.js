a1b27b225cbc6a2e91349c487f0c242369be67b9
Marta Szymańska <m.szymanska@mdbootstrap.com>
2017-10-31 21:04:44 +0100

    fixed sidenav

--- js/modules/vendor/waves.js
+++ js/modules/vendor/waves.js
@@ -206,14 +206,14 @@
             var parent = element.parentNode;
 
             // If input already have parent just pass through
-            if (parent.tagName.toLowerCase() === 'i' && parent.classList.contains('waves-effect')) {
+            if (parent.tagName.toLowerCase() === 'div' && parent.classList.contains('waves-effect')) {
                 return;
             }
 
             // Put element class and style to the specified parent
-            var wrapper = document.createElement('i');
-            wrapper.className = element.className + ' waves-input-wrapper';
-            element.className = 'waves-button-input';
+            var wrapper = document.createElement('div');
+            wrapper.className = 'waves-input-wrapper';
+            // element.className = element.className + ' waves-button-input';
 
             // Put element as child
             parent.replaceChild(wrapper, element);
@@ -224,8 +224,8 @@
             var color = elementStyle.color;
             var backgroundColor = elementStyle.backgroundColor;
 
-            wrapper.setAttribute('style', 'color:' + color + ';background:' + backgroundColor);
-            element.setAttribute('style', 'background-color:rgba(0,0,0,0);');
+            // wrapper.setAttribute('style', 'color:' + color + ';background:' + backgroundColor);
+            // element.setAttribute('style', 'background-color:rgba(0,0,0,0);');
 
         },
 
