233802ca870dcf27e12569f5fe0f2f6fce8b4cde
Bartek Malanowski <b.malanowski@mdbootstrap.com>
2018-01-04 07:06:29 +0100

    fixed scrolling navbar issue

--- js/modules/src/scrolling-navbar.js
+++ js/modules/src/scrolling-navbar.js
@@ -2,9 +2,11 @@
 const OFFSET_TOP = 50
 
 $(window).scroll(() => {
-  if ($('.navbar').offset().top > OFFSET_TOP) {
-    $('.scrolling-navbar').addClass('top-nav-collapse')
-  } else {
-    $('.scrolling-navbar').removeClass('top-nav-collapse')
+  if($('.navbar').length) {
+    if ($('.navbar').offset().top > OFFSET_TOP) {
+      $('.scrolling-navbar').addClass('top-nav-collapse')
+    } else {
+      $('.scrolling-navbar').removeClass('top-nav-collapse')
+    }
   }
 })
