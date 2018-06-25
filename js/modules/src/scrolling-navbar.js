85967873d1a750549c19c3ad85e35af6f58bdbb9
Bartek Malanowski <b.malanowski@mdbootstrap.com>
2018-06-25 14:46:34 +0200

    improved scrolling navbar

--- js/modules/src/scrolling-navbar.js
+++ js/modules/src/scrolling-navbar.js
@@ -1,12 +1,15 @@
 /* SCROLLING NAVBAR */
+
 const SCROLLING_NAVBAR_OFFSET_TOP = 50
 
-$(window).scroll(function () {
-  if($('.navbar').length) {
-    if ($('.navbar').offset().top > SCROLLING_NAVBAR_OFFSET_TOP) {
-      $('.scrolling-navbar').addClass("top-nav-collapse");
-    } else {
-      $('.scrolling-navbar').removeClass("top-nav-collapse");
+(function ($) {
+  $(window).on('scroll', function() {
+    if($('.navbar').length) {
+      if ($('.navbar').offset().top > SCROLLING_NAVBAR_OFFSET_TOP) {
+        $('.scrolling-navbar').addClass("top-nav-collapse");
+      } else {
+        $('.scrolling-navbar').removeClass("top-nav-collapse");
+      }
     }
-  }
-});
+  })
+})(jQuery);
